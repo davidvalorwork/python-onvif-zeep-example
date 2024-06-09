623b58ddd65ec624b8dc52d57e8494029f788f8d
David Valor <davidvalorwork@gmail.com>
2024-06-09 13:11:33 -0400

    s

--- src/utils/calculo_rotacion.py
+++ src/utils/calculo_rotacion.py
@@ -1,7 +1,7 @@
 import math
 from geopy.distance import geodesic
 
-def calcular_angulo_rotacion(origen, referencia, emergencia):
+def calcular_angulo_rotacion(origen, referencia, emergencia, coordenada_pan_referencia):
 
   # Formula de Haversine
   haversine_cateto_origen_referencia = geodesic(origen, referencia).meters
@@ -34,7 +34,16 @@ def calcular_angulo_rotacion(origen, referencia, emergencia):
   angulo_rotacion_grados_teorema_coseno = (teorema_del_coseno * 180) / math.pi
 
   print("angulo_rotacion_grados_teorema_coseno:",angulo_rotacion_grados_teorema_coseno)
-  return angulo_rotacion_grados_teorema_coseno
+
+  pan_transformada = (360 - angulo_rotacion) / 360
+  if(producto_cruz > 0):
+    pan_transformada = pan_transformada + coordenada_pan_referencia
+  else:
+    pan_transformada = pan_transformada - coordenada_pan_referencia
+
+  print(pan_transformada)
+
+  return pan_transformada
 
 
 
@@ -48,18 +57,16 @@ if __name__ == '__main__':
   berta = (10.365654678419954, -66.97793301987491)
   simoncito = (10.366454368101305, -66.97769878433466)
   pao = (10.366164343123057, -66.97679802169682)
-  coordenada_pan_1 = 0.565
+  coordenada_pan_referencia = 0.565
 
   # angulo_rotacion = calcular_angulo_rotacion(origen, referencia, bocono)
   # angulo_rotacion = calcular_angulo_rotacion(origen, referencia, meta)
   # angulo_rotacion = calcular_angulo_rotacion(origen, referencia, simoncito)
-  angulo_rotacion = calcular_angulo_rotacion(origen, referencia, berta)
+  angulo_rotacion = calcular_angulo_rotacion(origen, referencia, berta, coordenada_pan_referencia)
   # angulo_rotacion = calcular_angulo_rotacion(origen, referencia, pao)
 
   print("Angulo de rotación horizontal en sentido antihorario:", angulo_rotacion)
 
-  pan_transformada = (360 - angulo_rotacion) / 360
   print("Pan de 0 fuera el punto inicial de la camara:", pan_transformada)
 
-  pan_transformada = pan_transformada + coordenada_pan_1
   print("Pan de movimiento para la camara:", pan_transformada)
