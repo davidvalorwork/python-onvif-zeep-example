377516dc121a16ae31495d74f6720f94bf8b6bd0
David Valor <davidvalorwork@gmail.com>
2024-06-09 18:39:33 -0400

    Solved init

--- src/utils/onvif/init.py
+++ src/utils/onvif/init.py
@@ -12,9 +12,13 @@ class Onvif:
         # transport=Transport(timeout=self.timeout),
       )
       self.service = self.device.create_ptz_service()
+      self.ptz_service = self.device.create_ptz_service()
+      media_service = self.device.create_media_service()
+      self.media_profile = media_service.GetProfiles()[0]
       print("Conectado a la camara")
 
     def move(self,x, y):
+
         print("Moviendo", x, y)
         absolute_move_request = self.service.create_type("AbsoluteMove")
         pose_dict = {"PanTilt": {"x": x, "y": y}, "Zoom": {"x": 0.0}}
@@ -22,5 +26,6 @@ class Onvif:
         absolute_move_request.Position["PanTilt"]["x"] = x
         absolute_move_request.Position["PanTilt"]["y"] = y
         absolute_move_request.Position["Zoom"]["x"] = 0
-        self.device.AbsoluteMove(absolute_move_request)
+        absolute_move_request.ProfileToken = self.media_profile.token
+        self.ptz_service.AbsoluteMove(absolute_move_request)
         print("Movido a", x, y)
