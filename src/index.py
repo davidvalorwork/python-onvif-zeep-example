8c1a63a8094dcb48e012180483250894f0afaa9b
David Valor <davidvalorwork@gmail.com>
2024-06-09 13:42:22 -0400

    Finalizado

--- src/index.py
+++ src/index.py
@@ -16,6 +16,7 @@ def main(json_parameters):
     data["coordenada"] = (10.365006828332977, -66.97706278051929) # meta
     # data["coordenada"] = (10.365654678419954, -66.97793301987491) # berta
     # data["coordenada"] = (10.366164343123057, -66.97679802169682) # pao
+    # data["coordenada"] = (10.364618573568935, -66.97791360268778) # estacionamiento
     print(json.dumps(data, indent=4))
     camara = init_camera(data, type_camera)
     pan = calcular_angulo_rotacion(
@@ -26,7 +27,7 @@ def main(json_parameters):
     )
 
     print(camara)
-    camara.move(0.565,0.5)
+    # camara.move(0.565,0.5)
     camara.move(pan,0.5)
     # camara.move(0.565,0.5)
 
