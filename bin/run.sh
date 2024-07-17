08c6d7be5bf56a3746247339e1284d885a0329c4
David Valor <davidvalorwork@gmail.com>
2024-07-17 11:09:42 -0400

    READY

--- bin/run.sh
+++ bin/run.sh
@@ -2,7 +2,7 @@ docker stop onvif
 docker rm onvif
 docker rmi davidvalorwork/example-onvif
 docker build -t davidvalorwork/example-onvif .
-docker run -d --name onvif -t  -p 8080:8080 davidvalorwork/example-onvif
+# docker run -d --name onvif -t  -p 8080:8080 davidvalorwork/example-onvif
 # docker exec -it onvif bash
 
-# docker push davidvalorwork/example-onvif
+docker push davidvalorwork/example-onvif
